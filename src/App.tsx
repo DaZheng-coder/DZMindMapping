c94ab58897e068ea4499279fdcb587ce8c3f8b24
DESKTOP-HG68PD8\PPmaka <924370430@qq.com>
2024-01-13 17:59:23 +0800

    feat: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import MindMapping from "./components/MindMapping";
+
+function App() {
+  return <div>
+    <MindMapping />
+  </div>;
+}
+
+export default App;
---
+++ src/components/MindMapping/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.less';
+
+interface IMingMapping {}
+
+const MindMapping: React.FC<IMingMapping> = () => {
+  return <div>1324</div>
+};
+
+export default MindMapping;
